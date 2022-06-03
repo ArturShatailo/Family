eb3710e8950bebf10d5ba68c0a77d1f0f2e22ecb
Artur <Shatailo>
2022-06-03 23:34:08 +0300

    changed field names

--- src/Family.java
+++ src/Family.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 
 public class Family implements Names{
 
-    private Human mother;
-    private Human father;
+    private Human woman;
+    private Human man;
     private final ArrayList<Human> children = new ArrayList<>();
     private final ArrayList<Human> grannies = new ArrayList<>();
 
     public Family(Human mother, Human father) {
-        this.mother = mother;
-        this.father = father;
+        this.woman = mother;
+        this.man = father;
     }
 
     public Family() {
@@ -24,10 +24,10 @@ public class Family implements Names{
 
     //This method fills out grannies field with mother's and father's mother and father objects.
     public void familyTree(){
-        grannies.add(mother.getMother());
-        grannies.add(father.getFather());
-        grannies.add(father.getMother());
-        grannies.add(mother.getFather());
+        grannies.add(woman.getMother());
+        grannies.add(man.getFather());
+        grannies.add(man.getMother());
+        grannies.add(woman.getFather());
     }
 
     //Creates random amount of children (Human objects) and add them into children field of mother field and father field,
@@ -36,19 +36,19 @@ public class Family implements Names{
 
         for(int i=0; i<amount; i++){
             if (Tech.getRandom(0, 1) == 0) {
-                children.add(new Human(mother, father, "f"));
+                children.add(new Human(woman, man, "f"));
             } else {
-                children.add(new Human(mother, father, "m"));
+                children.add(new Human(woman, man, "m"));
             }
         }
-        mother.setChildren(children);
-        father.setChildren(children);
+        woman.setChildren(children);
+        woman.setChildren(children);
     }
 
     @Override
     public String toString(){
-        return "Mother: "+mother.toString()+
-                "\nFather: "+father.toString()+
+        return "Mother: "+woman.toString()+
+                "\nFather: "+man.toString()+
                 "\nChildren: "+ Arrays.toString(children.toArray())+
                 "\nGrannies: "+ Arrays.toString(grannies.toArray());
     }
